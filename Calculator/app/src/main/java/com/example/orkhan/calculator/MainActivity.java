0fd9307c4f493a7ecdccf131d5e947b00b7cfdc8
orkhan <ohuseynli2018@ada.edu.az>
2016-11-24 17:35:49 +0400

    working, have problems with layout

--- Calculator/app/src/main/java/com/example/orkhan/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/orkhan/calculator/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.orkhan.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //to do
     }
 }
